67e61b44d5da55de5cd8a124eb90ffe5228c5dbe
isabelajs <isabelajimenezs96@gmail.com>
2025-02-21 17:07:26 -0500

    refactor(AuthUseCase): integrate cache repository for session management and enhance login/register flow

--- src/User/application/use-cases/auth.usecase.ts
+++ src/User/application/use-cases/auth.usecase.ts
@@ -1,30 +1,35 @@
 import { User } from '../../domain/User';
 import { UserRepository } from '../../domain/repositories/User.repository';
-
+import { ICacheRepository } from '../../../Shared/domain/Repositories/cache-data.repository';
+import { EnumCacheData } from '../../../Shared/domain/enums/cache-data.enum';
 export class AuthUseCase {
   constructor(
     private readonly userRepository: UserRepository,
+    private readonly cacheRepository: ICacheRepository
   ) {}
 
-  async login(email: string, password: string): Promise<User> {
-    const user = await this.userRepository.checkUserbyEmail(email);
-    
-    if(!user){
-      throw new Error('User not found');
-    }
-    const newUser = new User(user.name, email, password);
-    
-    if (!user || !newUser.verifyPassword(password)) {
-      throw new Error('Invalid credentials');
-    }
-
+  async login(email: string, password: string): Promise<User | null> {
+    const user = await this.userRepository.login(email, password)
+    this.cacheRepository.set(EnumCacheData.SESSION, user);
     return user;
   }
 
   async register(name: string, email: string, password: string): Promise<void> {
     const user = new User(name, email, password);
-    await this.userRepository.save(user).catch((error) => {
+    await this.userRepository.register(user).catch((error) => {
       throw new Error(error.message);
     });
+    this.cacheRepository.set(EnumCacheData.SESSION, user);
   }
+
+  async getSession(): Promise<User | null> {
+    const user = await this.cacheRepository.get<User>(EnumCacheData.SESSION);
+    if(!user) return null;
+    return user;
+  }
+
+  async logout(): Promise<void> {
+    this.cacheRepository.remove(EnumCacheData.SESSION);
+  }
+
 }
