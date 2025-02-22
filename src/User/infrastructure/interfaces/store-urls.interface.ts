8d27408a8013470eb0b6ae602259890abf0ba94c
isabelajs <isabelajimenezs96@gmail.com>
2025-02-21 23:41:03 -0500

    feat(interfaces): add URL and stored user interfaces for user management

---
+++ src/User/infrastructure/interfaces/store-urls.interface.ts
@@ -0,0 +1,6 @@
+import { IUrls } from "../../domain/interfaces/urls.interface";
+
+export interface IStoreUrls {
+  email: string;
+  urls: IUrls[];
+}
---
+++ src/User/infrastructure/interfaces/stored-user.interface.ts
@@ -0,0 +1,5 @@
+export interface IStoredUser {
+    name: string;
+    email: string;
+    password: string;
+}
