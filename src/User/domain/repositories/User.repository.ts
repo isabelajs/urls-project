77a721a3538a53b84cd3e516afe6c20e1dfa913a
isabelajs <isabelajimenezs96@gmail.com>
2025-02-21 23:41:31 -0500

    feat(UserRepository): add methods for URL management by email

--- src/User/domain/repositories/User.repository.ts
+++ src/User/domain/repositories/User.repository.ts
@@ -1,7 +1,11 @@
+import { IUrls } from '../interfaces/urls.interface';
 import { User } from '../User';
 
 export abstract class UserRepository {
   abstract login(email: string, password: string): Promise<User | null>;
   abstract register(user: User): Promise<void>;
   abstract getUserByEmail(email: string): Promise<User | null>;
+  abstract saveUrlsByEmail(email: string, url: IUrls): Promise<void>;
+  abstract getUrlsByEmail(email: string): Promise<IUrls[]>;
+  abstract removeUrl(email: string, nameUrl: string): Promise<void>;
 }
