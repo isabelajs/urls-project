49fe7eec3114fa5f4c827dd85207824b649f4c2d
isabelajs <isabelajimenezs96@gmail.com>
2025-02-21 23:41:48 -0500

    feat(Home): enhance URL management with form validation and display messages

--- src/UI/pages/Home.tsx
+++ src/UI/pages/Home.tsx
@@ -1,15 +1,27 @@
-import { useAuthStore } from "../stores/authStore";
-import { HomeContainer, URLContainer } from "./Home.styles";
-import { Button, Input, Text } from "@isabelajs/design-system";
-import { InputContainer } from "./Login.styles";
+import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { useAuthStore } from "../stores/authStore";
+import {
+  CardsContainer,
+  HomeContainer,
+  MessageContainer,
+  URLContainer,
+} from "./Home.styles";
+import { Button, Card, Input, Text } from "@isabelajs/design-system";
+import { InputContainer } from "./Login.styles";
 import { TypeUrl } from "../interfaces/urls.type";
+import { useUrlsStore } from "../stores/urlsStore";
 
 function Home() {
-  const logout = useAuthStore((state) => state.logout);
+  const [error, setError] = useState("");
+  const [sucess, setSucess] = useState(false);
+  const { logout, user } = useAuthStore((state) => state);
+  const { addUrl, getUrls, removeUrl, urls } = useUrlsStore((state) => state);
+
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors, isValid },
   } = useForm<TypeUrl>({
     defaultValues: {
@@ -20,86 +32,143 @@ function Home() {
   });
 
   const onSubmit = async (data: TypeUrl) => {
-    console.log(data);
+    if (!user?.email) return;
+    try {
+      await addUrl(user?.email, data);
+      setError("");
+      setSucess(true);
+      reset();
+    } catch (error: any) {
+      setError(error.message);
+      setSucess(false);
+    }
   };
 
+  useEffect(() => {
+    if (error || sucess) {
+      setTimeout(() => {
+        setError("");
+        setSucess(false);
+      }, 1500);
+    }
+  }, [error, sucess]);
+
+  useEffect(() => {
+    const fetchUrls = async () => {
+      if (!user?.email) return;
+      await getUrls(user?.email);
+    };
+    fetchUrls();
+  }, []);
+
   return (
-    <HomeContainer>
-      <Button
-        text="Salir"
-        variant="outline"
-        onClick={logout}
-        customStyles={{ alignSelf: "flex-end" }}
-      />
-      <URLContainer>
-        <Text color="primary" fontWeight="bold" text="Urls" type="h1" />
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <InputContainer>
+    <>
+      <HomeContainer>
+        <Button
+          text="Salir"
+          variant="outline"
+          onClick={logout}
+          customStyles={{ alignSelf: "flex-end" }}
+        />
+        <URLContainer>
+          <Text color="primary" fontWeight="bold" text="Urls" type="h1" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <InputContainer>
+              <Text
+                color="primary"
+                fontWeight="bold"
+                text="URL para salvar"
+                type="body1"
+              />
+              <Controller
+                control={control}
+                name="url"
+                rules={{
+                  required: "URL es requerida",
+                  pattern: {
+                    value: /^https?:\/\/[^\s]+$/,
+                    message: "URL inválida",
+                  },
+                }}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    type="text"
+                    placeholder="URL"
+                    isValid={!errors.url && field.value !== ""}
+                    error={errors.url?.message || ""}
+                  />
+                )}
+              />
+            </InputContainer>
+            <InputContainer>
+              <Text
+                color="primary"
+                fontWeight="bold"
+                text="Nombre de URL"
+                type="body1"
+              />
+              <Controller
+                control={control}
+                name="name"
+                rules={{
+                  required: "Nombre de URL es requerido",
+                  minLength: {
+                    value: 3,
+                    message: "Nombre de URL debe tener al menos 3 caracteres",
+                  },
+                }}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    type="text"
+                    placeholder="Nombre de URL"
+                    isValid={!errors.name && field.value !== ""}
+                    error={errors.name?.message || ""}
+                  />
+                )}
+              />
+            </InputContainer>
+            <Button
+              text="Guardar"
+              type="submit"
+              variant="primary"
+              size="large"
+              disabled={!isValid}
+            />
+          </form>
+        </URLContainer>
+
+        <MessageContainer>
+          {sucess && (
             <Text
-              color="primary"
-              fontWeight="bold"
-              text="URL para salvar"
+              color="success"
+              text="URL guardada correctamente"
               type="body1"
             />
-            <Controller
-              control={control}
-              name="url"
-              rules={{
-                required: "URL es requerida",
-                pattern: {
-                  value: /^https?:\/\/[^\s]+$/,
-                  message: "URL inválida",
-                },
-              }}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  type="text"
-                  placeholder="URL"
-                  isValid={!errors.url && field.value !== ""}
-                  error={errors.url?.message || ""}
-                />
-              )}
-            />
-          </InputContainer>
-          <InputContainer>
-            <Text
-              color="primary"
-              fontWeight="bold"
-              text="Nombre de URL"
-              type="body1"
-            />
-            <Controller
-              control={control}
-              name="name"
-              rules={{
-                required: "Nombre de URL es requerido",
-                minLength: {
-                  value: 3,
-                  message: "Nombre de URL debe tener al menos 3 caracteres",
-                },
-              }}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  type="text"
-                  placeholder="Nombre de URL"
-                  isValid={!errors.name && field.value !== ""}
-                  error={errors.name?.message || ""}
-                />
-              )}
-            />
-          </InputContainer>
-          <Button
-            text="Guardar"
-            type="submit"
-            variant="primary"
-            size="large"
-            disabled={!isValid}
+          )}
+          {error && <Text color="error" text={error} type="body1" />}
+        </MessageContainer>
+      </HomeContainer>
+      <CardsContainer>
+        {urls.map((url) => (
+          <Card
+            key={url.url}
+            title={url.url}
+            description={url.name}
+            icon="RiDeleteBinLine"
+            customStylesIcon={{
+              color: "#F17474",
+              fontSize: "1rem",
+            }}
+            onAction={() => {
+              if (!user?.email) return;
+              removeUrl(user?.email, url.name);
+            }}
           />
-        </form>
-      </URLContainer>
-    </HomeContainer>
+        ))}
+      </CardsContainer>
+    </>
   );
 }
 
